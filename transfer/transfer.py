e2cba221100145bb78fcfd5fc4831b07be046268
Lourens <lourensros@gmail.com>
2021-02-27 13:05:28 +0200

    Return category name

--- transfer/transfer.py
+++ transfer/transfer.py
@@ -1,4 +1,8 @@
 from discord.ext import commands
+from discord import utils
+
+from core import checks
+from core.models import PermissionLevel
 
 
 class Transfer(commands.Cog):
@@ -6,8 +10,16 @@ class Transfer(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def say(self, ctx, *, message):
-        await ctx.send(message)
+    @commands.guild_only()
+    @checks.has_permission(PermissionLevel.REGULAR)
+    async def transfer(self, ctx, *, message):
+        """
+        Transfer a thread to a different category
+        """
+        content = message.content.replace(self.bot.prefix, "")
+        names = content.split(" ")
+        category = utils.get(ctx.guild.categories, id=names[1])
+        await ctx.send(category.name)
 
 
 def setup(bot):
